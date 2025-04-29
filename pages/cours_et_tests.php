1e734eec42ae1cdc4a64553ef2f6427d608112a2
erroubaai aya <erroubaaiaya318@gmail.com>
2025-04-29 02:00:43 +0100

    Erroubaai Aya commit8

--- pages/cours_et_tests.php
+++ pages/cours_et_tests.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cour et Test</title>
-    <link rel="stylesheet" href="../styles/cour_test.css">
+    <link rel="stylesheet" href="../styles/cour_et_test.css">
 <body>
 
     <div class="container">
